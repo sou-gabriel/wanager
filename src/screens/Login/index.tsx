1f923f30a3f552eb58b0c1d7a8f0a2566ba6fda5
Gabriel Ramos <dev.gabrielramos@gmail.com>
2023-01-26 22:55:51 -0300

    feat: add validations to login screen entries

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react'
 import { View } from 'react-native'
+import { Controller, useForm, useWatch } from 'react-hook-form'
 import { StackScreenProps } from '@react-navigation/stack'
 
 import { Input } from '../../components/Input'
@@ -10,11 +12,27 @@ import * as S from './styles'
 
 type LoginProps = StackScreenProps<RootStackParamList, 'Login'>
 
+type FormFieldNames = 'email' | 'password'
+
+type FormValues = {
+  email: string
+  password: string
+}
+
 export const Login = ({ navigation }: LoginProps) => {
+  const { control } = useForm<FormValues>()
+  const formFieldValues = useWatch({ control })
+  const [currentActiveInput, setCurrentActiveInput] =
+    useState<FormFieldNames>()
+
+  const formValues = Object.values(formFieldValues)
+  const isFormFilled =
+    formValues.length === 0 ? false : formValues.every(Boolean)
+
   return (
     <S.Container>
       <S.Header>
-        <GoBackButton onGoBack={navigation.goBack} />
+        <GoBackButton onGoBack={() => navigation.navigate('Home')} />
       </S.Header>
 
       <S.Content>
@@ -29,17 +47,44 @@ export const Login = ({ navigation }: LoginProps) => {
 
             <S.FormContent>
               <S.InputBlock>
-                <Input placeholder="E-mail" />
+                <Controller
+                  name="email"
+                  control={control}
+                  render={() => (
+                    <Input
+                      placeholder="E-mail"
+                      isActive={currentActiveInput === 'email'}
+                      onFocus={() => setCurrentActiveInput('email')}
+                      autoCapitalize="none"
+                    />
+                  )}
+                />
               </S.InputBlock>
 
               <S.InputBlock>
-                <Input placeholder="Senha" secureTextEntry />
+                <Controller
+                  name="password"
+                  control={control}
+                  render={() => (
+                    <Input
+                      placeholder="Senha"
+                      isActive={currentActiveInput === 'password'}
+                      onFocus={() => setCurrentActiveInput('password')}
+                      autoCapitalize="none"
+                      secureTextEntry
+                    />
+                  )}
+                />
               </S.InputBlock>
             </S.FormContent>
           </View>
 
           <S.FormFooter>
-            <Button title="Entrar" />
+            <Button
+              title="Entrar"
+              isActive={isFormFilled}
+              disabled={!isFormFilled}
+            />
           </S.FormFooter>
         </S.Form>
       </S.Content>
